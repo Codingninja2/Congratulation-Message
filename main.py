4fb40eedcfd129c9d0975a10d54977529cd38360
codingninja2 <edwinafatsawo@gmail.com>
2024-12-14 12:07:38 +0000

    first Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Input: Word in lowercase
+word = input("Enter the word 'congratulations' in lowercase: ")
+
+# Convert the word to uppercase
+uppercase_word = word.upper()
+
+# Output the result
+print("The word in uppercase is:", uppercase_word)
